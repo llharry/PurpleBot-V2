0c8f209f6eebbc8dc9973d5366539150862c28c2
llharry <llharry2@hotmail.co.uk>
2016-12-29 11:34:00 +0000

    Initial
    commit

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var assert = require('assert');
+var paridia = require('../lib');
+
+describe('paridia', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
